dab2aff4ecf13064a13bd717709f2eb826585cf7
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-18 21:10:42 -0600

    add config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+import  config from './utils/config'
+
 import express from 'express'
 
 const app = express()
-
-const port = 3000
 
 app.get('/', (req,res) => {
   res.send({ msg: 'Hello There' })
 })
 
-app.listen(port)
+app.listen(config.port)
