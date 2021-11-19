501e91727fd2955b06fc22d831823c153e95c496
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-18 22:03:49 -0600

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import  config from './utils/config'
-
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
 
+import  config from './utils/config'
 import logger from './utils/logger'
+
+
 
 const app = express()
 
+
 app.use(logger.middleware)
+app.use(helmet())
+app.use(cors({origin: config.origin,}))
 
 app.get('/', (req,res) => {
     logger.log.info("Test")
