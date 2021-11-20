2770224d5ad0076bb40241883a900ab5d21653dc
raguila6 <raguila6@stu.southtexascollege.edu>
2021-11-19 19:08:59 -0600

    finished

--- src/routes/api/todos.js
+++ src/routes/api/todos.js
@@ -1,12 +1,48 @@
 import { Router } from "express"
-
-import {getTodos} from '../../utils/todos'
+import {getTodos, getTodo, addTodos, updateTodo} from '../../utils/todos'
+import {NETWORK_AUTHENTICATION_REQUIRED, StatusCodes} from 'http-status-codes'
 
 const router=Router()
 
 router.get('/', (req,res) =>{
-    const Todos = getTodos()
+    const Todos = getTodos(req.query.completed)
     res.send(Todos)
+})
+
+router.get('/:id', (req, res, next) => {
+    const todo = getTodo(req.params.id)
+    if (todo){
+        res.send(todo)
+    } else {
+        res.status(StatusCodes.NOT_FOUND)
+        next(new Error('Not found'))
+    }
+})
+
+router.post('/', (req,res,next)=> {
+    const todo = req.body
+    const response = addTodos(todo)
+    if (response.error){
+        res.status(StatusCodes.BAD_REQUEST)
+        next(response.error)
+    } else{
+        res.status(StatusCodes.CREATED)
+        res.send(response.newTodo)
+    }
+        
+})
+
+router.put('/:id', (req, res, next) => {
+    const todo = req.body  
+    const response = updateTodo(req.params.id, todo)
+
+    if (response.error){
+        res.status(StatusCodes.BAD_REQUEST)
+        next(response.error)
+    } else {
+        res.status(StatusCodes.OK)
+        res.send(response.updateTodo)
+    }
 })
 
 export default router
--- src/utils/todos.js
+++ src/utils/todos.js
@@ -1,6 +1,14 @@
 import { nanoid } from "nanoid"
+import logger from "./logger"
+import Joi from "joi"
+
 
 const Todos = []
+
+const todoSchema = Joi.object({
+    text: Joi.string().min(10).required(),
+    completed: Joi.boolean().required(),
+})
 
 const baseTodo = {
     id: nanoid(),
@@ -10,10 +18,50 @@ const baseTodo = {
 
 Todos.push(baseTodo)
 
-export const getTodos = () => {
+export const getTodos = (completed = null) => {
+
+    if (completed === null) {
+    logger.log.success('Getting  all Todos')
     return Todos
+    } 
+        logger.log.success('Getting completion todos')
+        const isCompleted = completed === 'true' ? true : false
+        return todos.filter(todo => todo.completed === isCompleted)
+    
+}
+
+export const getTodo = (id) =>{
+    logger.log.success(`Getting todo with id: ${id}`)
+    return Todos.find((todo) => todo.id === id)
 }
 
 export const addTodos = (todo) => {
-    todos.push(todo)
+    logger.log.info(`Validating ${todo}`)
+    const {error} = todoSchema.validate(todo)
+
+    if (error){
+        logger.log.error(new Error(`Validation error: ${error.message}`))
+        return {error}
+    }
+
+    logger.log.success(`Validated: ${todo} to add` )
+    const newTodo = {id: nanoid(), ...todo}
+    Todos.push(newTodo)
+    return {newTodo}
+}
+
+export const updateTodo = (id,todo) =>{
+    logger.log.info(`Validating ${todo} for update`)
+    const {error} = todoSchema.validate(todo)
+
+    if (error){
+        logger.log.error(new Error(`Validation error: ${error.message}`))
+        return {error}
+    }
+
+    logger.log.success(`Validated: ${todo}`)
+    const todoIndex = todos.findIndex((t) => t.id === id)
+    Todos[todoIndex] = {id, ...todo}
+    const updatedTodo = todos[todoIndex]
+    return todos[todoIndex]
 }
